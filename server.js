366e5a191d3fa5c2588d5f7b3a8f0c2bf6d6f425
ANKIT GURWAN <109846346+AnkitGurwan@users.noreply.github.com>
2022-10-13 17:31:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const morgan=require('morgan');
-const bodyparser=require("body-parser");
-const path=require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
 
-const app=express();
+dotenv.config( { path : 'config.env'} )
+const connectDB = require('./server/database/connection');
 
-dotenv.config({path:'config.env'})
-const PORT=process.env.PORT
+const app = express();
 
+
+const PORT = process.env.PORT || 8080
+
+// log requests
 app.use(morgan('tiny'));
 
-app.use(bodyparser.urlencoded({extended:true}));
+// mongodb connection
+connectDB();
 
-app.set("view engine","ejs");
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.listen(PORT,()=>{
-    console.log('Server is running on the local port.');
-})
+// load routers
+app.use('/', require('./server/routes/routes'))
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
